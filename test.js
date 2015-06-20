3bcbd0d32b1bb4fb77e875ed6f0ec7aa2b1bb9ef
Rodrigo Morais <morais.rm@gmail.com>
2015-06-20 06:56:44 -0300

    Test 1 in JS.

---
+++ test.js
@@ -0,0 +1,15 @@
+﻿function solution(n) {
+
+    if (n <= 1) {
+        return n;
+    }
+    else {
+        return solution(n - 1) + (Math.pow(n, 2));
+    }
+};
+
+function calculate() {
+    var result = solution(parseInt(document.getElementById('number').value));
+
+    alert(result);
+};
